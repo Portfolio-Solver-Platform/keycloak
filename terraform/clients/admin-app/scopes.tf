74ee49184e9c08076ebc5776de951a425b24b180
Sofus Laubel <51532553+sofusl90@users.noreply.github.com>
2026-03-18 12:32:30 +0100

    feat: update scope assignments for solver-director clients (#18)

--- terraform/clients/admin-app/scopes.tf
+++ terraform/clients/admin-app/scopes.tf
@@ -5,7 +5,15 @@ resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
 
   optional_scopes = [
     var.scopes.solver_director.projects_read.name,
-    var.scopes.solver_director.projects_read_all.name
+    var.scopes.solver_director.projects_read_all.name,
+    var.scopes.solver_director.solvers_read.name,
+    var.scopes.solver_director.solvers_write.name,
+    var.scopes.solver_director.groups_read.name,
+    var.scopes.solver_director.groups_write.name,
+    var.scopes.solver_director.problems_read.name,
+    var.scopes.solver_director.problems_write.name,
+    var.scopes.solver_director.resources_read.name,
+    var.scopes.solver_director.resources_write.name,
   ]
 }
 
